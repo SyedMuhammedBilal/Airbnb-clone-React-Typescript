b5604e7d905a74a3b55ef4a043f9d08c70e97502
Syed Muhammed Bilal <intergo878@gmail.com>
2020-12-19 11:25:27 +0500

    new header component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import './Banner.css'
+
+const Banner: React.FC = () => {
+  return (
+    <div className='banner'>
+      <div className='banner__search'>
+        <Button variant='outlined'>Search Dates</Button>
+      </div>
+      <div className='banner__info'>
+        <h1>Get out stretch your imagination</h1>
+        <h5>
+          Plan a different kind of gateway to uncover the hidden gems near you.
+        </h5>
+        <Button variant='outlined'>Explore Nearby</Button>
+      </div>
+    </div>
+  )
+};
+
+export default Banner;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Banner from './Banner'
 import './Home.css'
 
 function Home() {
   return (
     <div className='home'>
       <h1>Home</h1>
+      <Banner />
     </div>
   )
-}
+};
  
 export default Home
